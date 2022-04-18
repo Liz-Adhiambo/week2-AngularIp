711938d48bc153a193a78b5aace8e4aeb7ee859b
Liz <adhiamboliz3@gmail.com>
2022-04-18 18:13:06 +0300

    Added  function to search for users

--- src/app/components/search/github.service.ts
+++ src/app/components/search/github.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { catchError, count, Observable, retry } from 'rxjs';
+import { CLIENT_ID, CLIENT_SECRET} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  //for github profile
+  public getProfile(searchQuery:Observable <any[]>){
+    let dataURL = `https://api.github.com/users/${searchQuery}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+    return this.httpClient.get(dataURL).pipe(
+    
+    );
+    
+  }
+
+  
+
+  public handleErrors(error:HttpErrorResponse){
+    let errorMessage:string;
+    if(error.error instanceof ErrorEvent){
+      //server side error
+      errorMessage=`MESSAGE: ${error.error.message}`;
+    }
+    else{
+      //client side error
+      errorMessage=`STATUS: ${error.status} MESSAGE : ${error.message}`;
+    }
+  
+
 }
+
+
+}
+
